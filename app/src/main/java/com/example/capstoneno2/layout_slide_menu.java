113184ccdc6b512624acffd031973abe62e8478c
jjj2646 <jji1232646@gmail.com>
2024-04-01 11:14:45 +0900

    Initial commit

---
+++ app/src/main/java/com/example/capstoneno2/layout_slide_menu.java
@@ -0,0 +1,14 @@
+package com.example.capstoneno2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class layout_slide_menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout_slide_menu);
+    }
+}
